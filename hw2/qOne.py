2ab2864ffb7a63107372e1693dd8a510241fa12e
nitron532 <89716148+nitron532@users.noreply.github.com>
2025-10-16 00:48:36 -0700

    up to question 4d

--- hw2/qOne.py
+++ hw2/qOne.py
@@ -61,64 +61,64 @@ def logWithFeatures(image, sigma, size=None):
     return (result,x,y)
 
 # #1.
-# blackSquareSm = np.ones((100,100))
-# blackSquareSm[50:80,50:80] = 0
-# toRotate = blackSquareSm
-# showMPL(blackSquareSm)
+blackSquareSm = np.ones((100,100))
+blackSquareSm[50:80,50:80] = 0
+toRotate = blackSquareSm
+showMPL(blackSquareSm)
 blackSquares = np.ones((512,512))
 blackSquares[60:100,60:100] = 0
 blackSquares[300:380,180:260] = 0
 blackSquares[100:220,300:420] = 0
 blackSquares[340:480,340:480] = 0
-# showMPL(blackSquares)
+showMPL(blackSquares)
 
 # #2.
 sigmaList = [1,2,4,8]
-# # for i in sigmaList:
-#     # harrisCorner(blackSquareSm, i, 4,1)
+for i in sigmaList:
+    harrisCorner(blackSquareSm, i, 4,1)
 
-# for i in sigmaList:
-#     result, x,y = logWithFeatures(blackSquareSm, i)
-#     # scale values to preserve the original LOG image result but with RGB channel added
-#     grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
-#     rgb = np.stack((grayNorm,)*3, axis = -1)
-#     showMPLDots(rgb,x,y)
+for i in sigmaList:
+    result, x,y = logWithFeatures(blackSquareSm, i)
+    # scale values to preserve the original LOG image result but with RGB channel added
+    grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
+    rgb = np.stack((grayNorm,)*3, axis = -1)
+    showMPLDots(rgb,x,y)
 
-# for i in sigmaList:
-#     #DOG by subtracting two diff gaussians of diff sigmas by factor k
-#     g1 = ndimage.gaussian_filter(blackSquareSm, i)
-#     g2 = ndimage.gaussian_filter(blackSquareSm, 1.4*i)
-#     result = g2-g1;
-#     compute = (result - result.min()) / (result.max() - result.min()) * 255
-#     x,y = topCoords(compute, 0.1)
-#     grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
-#     rgb = np.stack((grayNorm,)*3, axis = -1)
-#     showMPLDots(rgb,x,y)
+for i in sigmaList:
+    #DOG by subtracting two diff gaussians of diff sigmas by factor k
+    g1 = ndimage.gaussian_filter(blackSquareSm, i)
+    g2 = ndimage.gaussian_filter(blackSquareSm, 1.4*i)
+    result = g2-g1;
+    compute = (result - result.min()) / (result.max() - result.min()) * 255
+    x,y = topCoords(compute, 0.1)
+    grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
+    rgb = np.stack((grayNorm,)*3, axis = -1)
+    showMPLDots(rgb,x,y)
 
-# #3
-# toRotate= ndimage.rotate(toRotate, angle = 30, reshape = False,cval = 1, order = 0)
-# showMPL(toRotate)
+#3
+toRotate= ndimage.rotate(toRotate, angle = 30, reshape = False,cval = 1, order = 0)
+showMPL(toRotate)
 
-# for i in sigmaList:
-#     harrisCorner(toRotate, i, 4,1)
+for i in sigmaList:
+    harrisCorner(toRotate, i, 4,1)
 
-# for i in sigmaList:
-#     result, x,y = logWithFeatures(toRotate, i)
-#     # scale values to preserve the original LOG image result but with RGB channel added
-#     grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
-#     rgb = np.stack((grayNorm,)*3, axis = -1)
-#     showMPLDots(rgb,x,y)
+for i in sigmaList:
+    result, x,y = logWithFeatures(toRotate, i)
+    # scale values to preserve the original LOG image result but with RGB channel added
+    grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
+    rgb = np.stack((grayNorm,)*3, axis = -1)
+    showMPLDots(rgb,x,y)
 
-# for i in sigmaList:
-#     #DOG by subtracting two diff gaussians of diff sigmas by factor k
-#     g1 = ndimage.gaussian_filter(toRotate, i)
-#     g2 = ndimage.gaussian_filter(toRotate, 1.4*i)
-#     result = g2-g1;
-#     compute = (result - result.min()) / (result.max() - result.min()) * 255
-#     x,y = topCoords(compute, 0.1)
-#     grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
-#     rgb = np.stack((grayNorm,)*3, axis = -1)
-#     showMPLDots(rgb,x,y)
+for i in sigmaList:
+    #DOG by subtracting two diff gaussians of diff sigmas by factor k
+    g1 = ndimage.gaussian_filter(toRotate, i)
+    g2 = ndimage.gaussian_filter(toRotate, 1.4*i)
+    result = g2-g1;
+    compute = (result - result.min()) / (result.max() - result.min()) * 255
+    x,y = topCoords(compute, 0.1)
+    grayNorm = (result - result.min()) / (result.max()-result.min() + 1e-8)
+    rgb = np.stack((grayNorm,)*3, axis = -1)
+    showMPLDots(rgb,x,y)
 
 
 #4
@@ -133,22 +133,22 @@ for i in sigmaList:
     downSampledGaussians.append(blurred) 
     bsDown = downSampleByHalf(blurred) 
 
-# for i in gaussians:
-#     showMPL(i)
-# for i in downSampledGaussians:
-#     showMPL(i)
+for i in gaussians:
+    showMPL(i)
+for i in downSampledGaussians:
+    showMPL(i)
 index = 0
-# for i in gaussians:
-#     harrisCorner(i,-1,16,4)
-# for i in downSampledGaussians:
-#     if index >= 2:
-#         if index == 4:
-#             harrisCorner(i,-1,4,1)
-#         else:
-#             harrisCorner(i,-1,16,1)
-#     else:
-#         harrisCorner(i,-1,16,3)
-#     index+=1
+for i in gaussians:
+    harrisCorner(i,-1,16,4)
+for i in downSampledGaussians:
+    if index >= 2:
+        if index == 4:
+            harrisCorner(i,-1,4,1)
+        else:
+            harrisCorner(i,-1,16,1)
+    else:
+        harrisCorner(i,-1,16,3)
+    index+=1
 
 #c
 for i in range(1,5):
